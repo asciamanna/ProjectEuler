1567d12cc2591835b2f844315d6087f007a01653
Anthony Sciamanna <asciamanna@gmail.com>
2011-05-05 19:44:51 -0400

    some changes to the euler problems main program

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace ProjectEuler {
   class Program {
     static void Main(string[] args) {
-      //new Problem1(1000).Solve();
+      new Problem1(1000).Solve();
       new Problem2().Solve();
     }
   }
