dd8879a9a2b0c877d596076929b61a11bdfa05a1
Anthony Sciamanna <asciamanna@gmail.com>
2011-05-08 08:21:53 -0400

    changes to console display

--- ProjectEuler/Problem.cs
+++ ProjectEuler/Problem.cs
@@ -9,7 +9,7 @@ namespace ProjectEuler {
       startTime = DateTime.Now;
       var answer = this.Solve();
       endTime = DateTime.Now;
-      Console.WriteLine(string.Format("The answer to Project Euler Problem: {0}",answer));
+      Console.WriteLine(string.Format("The answer to Project Euler {0}: {1}", this.GetType().Name, answer));
       Console.WriteLine(string.Format("Total Seconds to Run: {0}", (endTime - startTime).TotalSeconds));
       //keep the console open.
       Console.ReadLine();
