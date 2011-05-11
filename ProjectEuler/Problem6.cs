b971c0c9d25a7ade852c4a62e5700f88b42e6b9f
Anthony Sciamanna <asciamanna@gmail.com>
2011-05-11 12:19:54 -0400

    stubbed out problem 6

---
+++ ProjectEuler/Problem6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler {
+  public class Problem6 : Problem {
+    //-----------------------------
+    //Find the difference between the sum of 
+    //the squares of the first one hundred natural
+    //numbers and the square of the sum.
+    //------------------------------
+
+    public override long Solve() {
+      throw new NotImplementedException();
+    }
+  }
+}
