519fce8cf37a52d0336f810646e84167eb4c77e7
Anthony Sciamanna <asciamanna@gmail.com>
2011-05-18 13:00:09 -0400

    stubbed problem 15

--- ProjectEuler/Problem15.cs
+++ ProjectEuler/Problem15.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace ProjectEuler {
   public class Problem15 : Problem {
+    readonly int gridSize;
+    public Problem15(int gridSize) {
+      this.gridSize = gridSize;
+    }
     public override long Solve() {
       throw new NotImplementedException();
     }
