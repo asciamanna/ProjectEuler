8e8fca64ba42ce81041dfed7f938c277d6647fea
Anthony Sciamanna <asciamanna@gmail.com>
2011-05-18 21:46:20 -0400

    stubbed out problem 16

---
+++ ProjectEuler/Problem16.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler {
+  public class Problem16 : Problem {
+    readonly int number;
+    readonly int exponent;
+
+    public Problem16(int number, int exponent) {
+      this.number = number;
+      this.exponent = exponent;
+    }
+    public override long Solve() {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ ProjectEulerTests/Problem16Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ProjectEuler;
+
+namespace ProjectEulerTests {
+  [TestFixture]
+  public class Problem16Test {
+    [Test]
+    public void Example() {
+      //Assert.AreEqual(26, new Problem16(2, 15).Solve());
+    }
+  }
+}
