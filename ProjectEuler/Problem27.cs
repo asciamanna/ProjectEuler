504c528d4f944dcd072fb9d7716be0a4e307ecd4
Anthony Sciamanna <asciamanna@gmail.com>
2011-05-30 19:11:38 -0400

    stubbed out problem 27

---
+++ ProjectEuler/Problem27.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler {
+  [EulerProblem(27, "Find a quadratic formula that produces the maximum number of primes for consecutive values of n.")]
+  public class Problem27 : Problem {
+    public override long Solve() {
+      throw new NotImplementedException();
+    }
+  }
+}
