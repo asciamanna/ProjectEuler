f5b59fbd56ccb897109f98ad61d493a4166fff41
Anthony Sciamanna <asciamanna@gmail.com>
2011-05-30 09:54:34 -0400

    started work on problem runner

---
+++ ProblemRunner/ProblemRunner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace ProblemRunner {
+  public class ProblemRunner {
+    int problemToRun;
+    DateTime start;
+    DateTime end;
+
+    public ProblemRunner(int problemToRun) {
+      this.problemToRun = problemToRun;
+    }
+
+    public void Run() {
+      var assembly = Assembly.LoadFrom(@"C:\development\ProjectEuler\ProjectEuler\bin\debug\ProjectEuler.exe");
+      var type = assembly.GetTypes().Where(t => t.Name == string.Format("Problem{0}", problemToRun)).First();
+      var problem = assembly.CreateInstance(type.FullName);
+      var method = type.GetMethod("SolveToConsole");
+      method.Invoke(problem, null);
+      Console.ReadLine();
+    }
+  }
+}
---
+++ ProblemRunner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemRunner {
+   class Program {
+    [STAThread] // for OLE
+    static void Main(string[] args) {
+      Console.Write("Which Project Euler problem do you want to solve (Enter a for all): ");
+      var input = Console.ReadLine().Trim();
+      int problemToRun = 0;
+      if (input.ToLower() == "a") {
+        //run all
+      }
+      else if (input.ToLower().StartsWith("slowest")) {
+        //print slowest to screen and file
+      }
+      else if (int.TryParse(input, out problemToRun)) {
+        new ProblemRunner(problemToRun).Run();
+      }
+    }
+  }
+}
