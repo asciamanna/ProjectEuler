49ebb56f8c22998aa25bbf2e351c946ef3a3556c
Anthony Sciamanna <asciamanna@gmail.com>
2011-06-09 22:09:15 -0400

    created permutation generator

--- ProjectEuler/Permutations.cs
+++ ProjectEuler/Permutations.cs
@@ -4,6 +4,27 @@ using System.Linq;
 using System.Text;
 
 namespace ProjectEuler {
-  public static class Permutations {
+  public static class Permutations<T> {
+
+    public static List<List<T>> Generate(List<T> list) {
+      var items = list;
+      List<List<T>> listOfPermutations = new List<List<T>>();
+      int i = 0;
+      while (i < items.Count) {
+        for (int j = 0; j < items.Count - 1; j++) {
+          items = swap(items, j, j + 1);
+          listOfPermutations.Add(new List<T>(items));
+        }
+        i++;
+      }
+      return listOfPermutations;
+    }
+
+    static List<T> swap(List<T> list, int i, int j) {
+      T nextItem = list[j];
+      list[j] = list[i];
+      list[i] = nextItem;
+      return list;
+    }
   }
 }
---
+++ ProjectEulerTests/PermutationsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ProjectEuler;
+
+namespace ProjectEulerTests {
+  [TestFixture]
+  public class PermutationsTest {
+    [Test]
+    public void Generate() {
+      var permutations = Permutations<char>.Generate(new List<char> { 'a', 'b', 'c' });
+      Assert.Contains(new List<char> { 'a', 'b', 'c' }, permutations);
+      Assert.Contains(new List<char> { 'b', 'a', 'c' }, permutations);
+      Assert.AreEqual(6, permutations.Count);
+    }
+  }
+}
