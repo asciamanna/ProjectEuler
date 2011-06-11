0064ae7d19a8c08ad18c7c750b94a53ae00f5f46
Anthony Sciamanna <asciamanna@gmail.com>
2011-06-11 12:52:23 -0400

    started problem 37

---
+++ ProjectEuler/Truncate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler {
+  public enum TruncateMethod {
+    LeftToRight,
+    RightToLeft,
+  }
+
+  public class Truncate {
+    TruncateMethod method;
+    string numberString;
+    public Truncate(long number, TruncateMethod method) {
+      this.numberString = number.ToString();
+      this.method = method;
+    }
+
+    public bool CanTruncate() {
+      return numberString.Length > 1;
+    }
+
+    public long CurrentValue() {
+      return long.Parse(numberString);
+    }
+
+    public long NextValue() {
+      if (!CanTruncate()) { throw new Exception("Cannot truncate a digit number any further"); }
+      int indexToRemove;
+      if (method == TruncateMethod.LeftToRight) {
+        indexToRemove = 0;
+      }
+      else {
+        indexToRemove = numberString.Length - 1;
+      }
+      numberString = numberString.Remove(indexToRemove, 1);
+      return long.Parse(numberString);
+    }
+  }
+}
---
+++ ProjectEulerTests/TruncateTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ProjectEuler;
+
+namespace ProjectEulerTests {
+  [TestFixture]
+  public class TruncateTest {
+    [Test]
+    public void Truncate_Left_To_Right() {
+      var truncate = new Truncate(234, TruncateMethod.LeftToRight);
+      Assert.IsTrue(truncate.CanTruncate());
+      Assert.AreEqual(34, truncate.NextValue());
+      Assert.IsTrue(truncate.CanTruncate());
+      Assert.AreEqual(4, truncate.NextValue());
+      Assert.IsFalse(truncate.CanTruncate());
+      Assert.AreEqual(4, truncate.CurrentValue());
+    }
+
+    [Test]
+    public void Truncate_Right_To_Left() {
+      var truncate = new Truncate(345, TruncateMethod.RightToLeft);
+      Assert.IsTrue(truncate.CanTruncate());
+      Assert.AreEqual(34, truncate.NextValue());
+      Assert.IsTrue(truncate.CanTruncate());
+      Assert.AreEqual(3, truncate.NextValue());
+      Assert.IsFalse(truncate.CanTruncate());
+      Assert.AreEqual(3, truncate.CurrentValue());
+    }
+  }
+}
