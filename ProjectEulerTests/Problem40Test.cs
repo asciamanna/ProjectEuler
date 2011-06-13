ba79bbfc27ec3a6a0e186ea6288ffd6f745a47a8
Anthony Sciamanna <asciamanna@gmail.com>
2011-06-13 13:10:16 -0400

    test for problem 40

--- ProjectEulerTests/Problem40Test.cs
+++ ProjectEulerTests/Problem40Test.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using ProjectEuler;
 
 namespace ProjectEulerTests {
   [TestFixture]
   public class Problem40Test {
     [Test]
     public void Solve() {
-
+      Assert.AreEqual(210, new Problem40().Solve());
     }
   }
 }
