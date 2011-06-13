943eb199b12b39a1ec784b25003e78cea2fad82d
Anthony Sciamanna <asciamanna@gmail.com>
2011-06-13 16:26:02 -0400

    started work on problem 41

--- ProjectEuler/Problem41.cs
+++ ProjectEuler/Problem41.cs
@@ -7,7 +7,12 @@ namespace ProjectEuler {
  [EulerProblem(41, "What is the largest n-digit pandigital prime that exists?")]
  public class Problem41 : Problem {
    public override long Solve() {
-     Primes.BuildPrimesLookup(987654322);
+     for (int i = 9; i > 0; i--) {
+       var pandigitals = Pandigitals.Generate(i).OrderByDescending(p => p);
+       foreach (var pandigital in pandigitals) {
+         if (Primes.IsPrime(pandigital)) return pandigital;
+       }
+     }
      return 0;
    }
  }
--- ProjectEulerTests/PandigitalsTest.cs
+++ ProjectEulerTests/PandigitalsTest.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using ProjectEuler;
 
 namespace ProjectEulerTests {
   [TestFixture]
   public class PandigitalsTest {
     [Test]
     public void Generate() {
+      var pandigitals1 = Pandigitals.Generate(1);
+      Assert.Contains(1, pandigitals1);
+      var pandgitals2 = Pandigitals.Generate(2);
+      Assert.Contains(12, pandgitals2);
+      Assert.Contains(21, pandgitals2);
+
+      var pandigitals9 = Pandigitals.Generate(9);
+      Assert.AreEqual(987654321, pandigitals9.Max());
 
     }
   }
