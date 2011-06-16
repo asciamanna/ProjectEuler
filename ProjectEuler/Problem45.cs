22945b4e47243f734c3b6baf1d8a0b4d0d234641
Anthony Sciamanna <asciamanna@gmail.com>
2011-06-16 17:54:29 -0400

    solution to problem 45

--- ProjectEuler/Problem45.cs
+++ ProjectEuler/Problem45.cs
@@ -5,14 +5,13 @@ using System.Text;
 using System.Numerics;
 
 namespace ProjectEuler {
-  //1,533,776,805
   [EulerProblem(45, "After 40755, what is the next triangle number that is also pentagonal and hexagonal?")]
   public class Problem45 : Problem {
     //start at T286 since T285 was first triangle number that is also hexagonal and pentagonal.
     const int startingTriangleNumber = 286;
 
     public override long Solve() {
-      for (int i = startingTriangleNumber; i < int.MaxValue; i++) {
+      for (long i = startingTriangleNumber; i < long.MaxValue; i++) {
         var triangleNumber = GenerateTriangleNumber(i);
         if (triangleNumber.IsHexagonal() && triangleNumber.IsPentagonal()) {
           return long.Parse(triangleNumber.ToString());
@@ -21,7 +20,7 @@ namespace ProjectEuler {
       throw new Exception("Cannot find triangle number that is pentagonal and hexagonal under: " + int.MaxValue);
     }
 
-    long GenerateTriangleNumber(int n) {
+    public long GenerateTriangleNumber(long n) {
       return (long)((n * (n + 1)) / 2);
     }
   }
--- ProjectEulerTests/Problem45Test.cs
+++ ProjectEulerTests/Problem45Test.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using ProjectEuler;
 
 namespace ProjectEulerTests {
   [TestFixture]
   public class Problem45Test {
     [Test]
     public void Solve() {
+      Assert.AreEqual(1533776805, new Problem45().Solve());
+    }
 
+    [Test]
+    public void GenerateTriangleNumber() {
+      Assert.AreEqual(40755, new Problem45().GenerateTriangleNumber(285));
     }
   }
 }
