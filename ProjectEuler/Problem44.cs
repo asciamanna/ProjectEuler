134f10875fa8cdae7fc87599a2f1e4bb2c82f2ea
Anthony Sciamanna <asciamanna@gmail.com>
2011-06-15 22:36:19 -0400

    stubbed out problem 44

--- ProjectEuler/Problem44.cs
+++ ProjectEuler/Problem44.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 
 namespace ProjectEuler {
+ [EulerProblem(44, "Find the smallest pair of pentagonal numbers whose sum and difference is pentagonal.")]
   public class Problem44 : Problem {
     public override long Solve() {
       throw new NotImplementedException();
