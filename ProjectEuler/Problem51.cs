68d1b97a692e0ef999346b084c0e6d212a32d8ce
Anthony Sciamanna <asciamanna@gmail.com>
2011-06-30 14:08:21 -0400

    started problem 51

---
+++ ProjectEuler/Problem51.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler {
+  [EulerProblem(51, "Find the smallest prime which, by changing the same part of the number, can form eight different primes.")]
+  public class Problem51 : Problem {
+    readonly int primeFamily;
+    readonly HashSet<long> primes;
+    public Problem51() : this(8) { }
+
+    public Problem51(int primeFamily) {
+      this.primeFamily = primeFamily;
+      primes = Primes.CalculatePrimesBelow(999999);
+    }
+    public override long Solve() {
+      var length = 5;
+      var sameDigits = 2;
+      var foundPrimes = FilterPrimes(length, sameDigits);
+      return foundPrimes.Count;
+    }
+
+    HashSet<long> FilterPrimes(int length, int sameDigits) {
+      throw new NotImplementedException();
+    }
+  }
+}
