495f35a37cbaa124a0a53d4e12580104a50073e0
Anthony Sciamanna <asciamanna@gmail.com>
2011-09-15 13:38:10 -0400

    started implementing problem 58

---
+++ ProjectEuler/Problem58.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler {
+  public class Problem58 : Problem {
+    decimal numberOfPrimes = 0;
+    decimal numberOfCornerNumbers = 1;
+    int currentCornerNumber = 1;
+
+    public override long Solve() {
+      int sideLength = 1;
+
+      while (RatioOfPrimes >= 0.1M || numberOfPrimes == 0) {
+        for (int i = 1; i <= 4; i++) {
+          var primetoCheck = currentCornerNumber += 2 * sideLength;
+          if (Primes.IsPrime(primetoCheck)) {
+            numberOfPrimes++;
+          }
+          numberOfCornerNumbers++;
+        }
+        sideLength++;
+      }
+      return sideLength;
+    }
+
+    decimal RatioOfPrimes {
+      get {
+        return numberOfPrimes / numberOfCornerNumbers;
+      }
+    }
+  }
+}
