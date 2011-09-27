6528a11da3b640202869b56d9961891f15b49caa
Anthony Sciamanna <asciamanna@gmail.com>
2011-09-27 13:21:51 -0400

    added problem 59 setup

---
+++ ProjectEuler/Problem59.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler {
+  public class Problem59 : Problem {
+    public override long Solve() {
+      throw new NotImplementedException();
+    }
+  }
+}
